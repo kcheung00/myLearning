9aaa934024e43efce8ec6ba880eda06ca6e2cf70
kcheung00 <kcheung00@hotmail.com>
2024-08-30 13:45:50 -0700

    new task component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,25 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './user/dummy-users';
+import { TaskComponent } from "./task/task.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS; // expose user to all components
+  selectedUserId = 'u1';
+
+  get selectedUser(){
+    return this.users.find( (user) => user.id === this.selectedUserId )!;
+  }
 
   onSelectUser( id:string ){
     console.log("Selected user with ID : " + id);
-
+    this.selectedUserId = id;
   }
 }
